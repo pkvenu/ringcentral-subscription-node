2b8aef6c94c24dbda9b0367f0385d55802e664de
pkvenu@gmail.com <paw1983$10>
2017-01-23 11:50:38 -0800

    initial code push

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# ringcentral-subscription-node
+![RingCentral Developer Platform](https://raw.githubusercontent.com/bdeanindy/ringcentral-subscription-basics/master/rc4d_logo_2016.png)
+
+# RingCentral Subscription Basic Demo App
+
+This is a demo application showing how to use RingCentral Push Notifications using the JS SDK. This application will GET all the Extensions (Enabled + User) in your RingCentral account (based on the environment, but defaults to Sandbox) and will add eventFilters in a single Subscription that will then receive Presence Events for these Extensions. The data from these presence events is then logged in the console of the application for you to review, and if you choose, filter and then execute external REST requests using the embedded HTTP server.
+
+This is only a demo application to show how to use Subscriptions properly with the [RingCentral-JS](https://github.com/ringcentral/ringcentral-js) SDK. This code should not be used directly in production, but can be used for PoC development and testing.
+
+You are able to configure this application to operate with either your RingCentral Sandbox or Production applications in the `.env` file by setting the `RC_ENV` value to either `sandbox` or `production`.
+
+You can also set this application to use any of the RingCentral Subscription Notification Types (they are all enabled by default) in the `.env` file. Read the API Reference for [Notification Event Types](https://developers.ringcentral.com/api-docs/latest/index.html#!#EventTypes.html) to understand more about these options.
+
+## Prerequisites
+
+* Valid RingCentral Account
+* Access to [RingCentral Developer Portal](https://developer.ringcentral.com)
+* Have created a RingCentral Sandbox Account for development. [Read how to do this here](https://developers.ringcentral.com/library/tutorials/test-account.html)
+* Have configured your Sandbox Account to have one or more extensions which allow presence to be monitored. [Read how to setup Presence monitoring here](http://success.ringcentral.com/articles/en_US/RC_Knowledge_Article/How-to-choose-specific-user-extensions-to-monitor-for-Presence)
+* Node.js installed locally
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Local Setup
+
+1. Clone `git clone https://github.com/bdeanindy/ringcentral-subscription-basics` this repository and `cd ringcentral-subscription-basics` into the project directory
+2. Install the dependencies `npm install`
+3. Configure your environment `cp .env.tmpl .env`
+4. Open the `.env` file and copy/paste your API keys and user credentials (admin user) into this file, then save/close it. 
+5. Create an application in [RingCentral Developer Portal](https://developers.ringcentral.com/my-account.html#/create-app) with the following parameters:
+    * Platform Type: Server-only (No UI)
+    * Application Type: Private
+    * Permissions Needed: Read Accounts, Read Call Log, Read Presence
+    * Name/Description: What you choose, but I always use something easy to identify in lists
+
+
+## Operation
+
+1. To start the application, `npm start`
+2. When you see "Subscription created successfully", you're ready to create a presence event to test
+3. Call one of the numbers you have subscribed to in the application (currently this subscribes to all Enabled User extensions in your RingCentral Sandbox account) from your cellular or an outside line.
+4. You should see an incoming msg (Notification) when you dial into one of these numbers.
+
+## Troubleshooting
+
+1. Not seeing presence events? Uncomment the console.log() which references `eventFilters` and re-run the application to see what extensions are being monitored
+2. Subscription failing to setup? Have you setup extensions in your RingCentral Sandbox account? Have they been enabled to monitor for Presence events?
+3. Anything else...add an issue and I will respond as soon as I am able
---
+++ index.js
@@ -0,0 +1,204 @@
+'use strict';
+
+// Deps
+if('production' !== process.env.NODE_ENV) require('dotenv').config(); // If using this in multi-environment (local and deployed versions) might want to add logic for checking NODE_ENV environment variable to load only if local
+var RC = require('ringcentral');
+var http = require('http');
+var util = require('util');
+
+// Set the RingCentral API Base URL based upon environment
+var rcServer = ('production' === process.env.RC_ENV)
+    ? 'https://platform.ringcentral.com'
+    : 'https://platform.devtest.ringcentral.com'
+    ;
+
+// Initialize the RC SDK
+var sdk = new RC({
+    server: rcServer,
+    appKey: process.env.RC_APP_KEY,
+    appSecret: process.env.RC_APP_SECRET,
+    cachePrefix: process.env.RC_CACHE_PREFIX
+});
+
+// APP VARS
+var server = http.createServer();
+var platform = sdk.platform();
+var subscription = sdk.createSubscription();
+
+// Login to the RingCentral Platform
+function login() {
+    return platform.login({
+            username: process.env.RC_USERNAME,
+            password: process.env.RC_PASSWORD,
+            extension: process.env.RC_EXTENSION
+        })
+        .then(function (response) {
+            console.log("Succesfully logged into the RC Account");
+            //console.log("The RC auth object is :", JSON.stringify(response.json(), null, 2));
+            init();
+        })
+        .catch(function (e) {
+            console.error("ERROR: ", e);
+            throw e;
+        });
+}
+
+login();
+
+function init() {
+    var extensions = [];
+    var page = 1;
+
+    function getExtensionsPage() {
+
+        return platform
+            .get('/account/~/extension/', {
+                type: 'User',
+                status: 'Enabled',
+                page: page,
+                perPage: process.env.EXTENSIONS_PER_PAGE //REDUCE NUMBER TO SPEED BOOTSTRAPPING
+            })
+            .then(function (response) {
+                //console.log("The extension response contained:", JSON.stringify(response.json(), null, 2));
+                var data = response.json();
+                //console.log("************** THE NUMBER OF EXTENSIONS ARE : ***************", data.records.length);
+                extensions = extensions.concat(data.records);
+                if (data.navigation.nextPage) {
+                    page++;
+                    return getExtensionsPage(); // this will be chained
+                } else {
+                    return extensions; // this is the finally resolved thing
+                }
+            });
+
+    }
+
+    /*
+     Loop until you capture all extensions
+     */
+    return getExtensionsPage()
+        .then(createEventFilter)
+        .then(startSubscription)
+        .catch(function (e) {
+            console.error(e);
+            throw e;
+        });
+
+}
+
+function createEventFilter(extensions) {
+    //console.log("********* CREATING EVENT FILTERS ***************");
+    var _eventFilters = [];
+    var messageType = messageType || 'SMS'; // SMS is the default message type in RC-API as of 2016-09-16
+    for(var i = 0; i < extensions.length; i++) {
+        var extension = extensions[i];
+        //console.log('EXTENSION: ', extension);
+        if(process.env.SUBSCRIBE_TO_EXTENSION_EVENTS) _eventFilters.push('/account/~/extension/' + extension.id);
+        if(process.env.SUBSCRIBE_TO_PRESENCE_EVENTS) _eventFilters.push('/account/~/extension/' + extension.id + '/presence?detailedTelephonyState=true&aggregated=true');
+        if(process.env.SUBSCRIBE_TO_MESSAGE_STORE_EVENTS) _eventFilters.push('/account/~/extension/' + extension.id + '/message-store');
+        if(process.env.SUBSCRIBE_TO_PRESENCE_LINE_EVENTS) _eventFilters.push('/account/~/extension/' + extension.id + '/presence/line');
+        if(process.env.SUBSCRIBE_TO_INSTANT_MESSAGE_EVENTS) _eventFilters.push('/account/~/extension/' + extension.id + '/message-store/instant?type=' + messageType);
+    }
+    console.log('EVENT FILTERS: ', _eventFilters);
+    return _eventFilters;
+}
+
+function startSubscription(eventFilters) { //FIXME MAJOR Use devices list somehow
+    //console.log("********* STARTING TO CREATE SUBSCRIPTION ON ALL FILTERED DEVICES ***************");
+    return subscription
+        .setEventFilters(eventFilters)
+        .register();
+}
+
+// Register Platform Event Listeners
+platform.on(platform.events.loginSuccess, handleLoginSuccess);
+platform.on(platform.events.loginError, handleLoginError);
+platform.on(platform.events.logoutSuccess, handleLogoutSuccess);
+platform.on(platform.events.logoutError, handleLogoutError);
+platform.on(platform.events.refreshSuccess, handleRefreshSuccess);
+platform.on(platform.events.refreshError, handleRefreshError);
+
+// Register Subscription Event Listeners
+subscription.on(subscription.events.notification, handleSubscriptionNotification);
+subscription.on(subscription.events.removeSuccess, handleRemoveSubscriptionSuccess);
+subscription.on(subscription.events.removeError, handleRemoveSubscriptionError);
+subscription.on(subscription.events.renewSuccess, handleSubscriptionRenewSuccess);
+subscription.on(subscription.events.renewError, handleSubscriptionRenewError);
+subscription.on(subscription.events.subscribeSuccess, handleSubscribeSuccess);
+subscription.on(subscription.events.subscribeError, handleSubscribeError);
+
+// Define Event Handlers
+function handleSubscriptionNotification(msg) {
+    console.log('SUBSCRIPTION NOTIFICATION.....');
+    console.log(util.inspect(msg, {showHidden: true, depth: null}));
+}
+
+function handleRemoveSubscriptionSuccess(data) {
+    console.log('REMOVE SUBSCRIPTION SUCCESS DATA: ', data);
+}
+
+function handleRemoveSubscriptionError(data) {
+    console.log('REMOVE SUBSCRIPTION ERROR DATA: ', data);
+}
+
+function handleSubscriptionRenewSuccess(data) {
+    console.log('RENEW SUBSCRIPTION SUCCESS DATA: ', data);
+}
+
+function handleSubscriptionRenewError(data) {
+    console.log('RENEW SUBSCRIPTION ERROR DATA: ', data);
+}
+
+function handleSubscribeSuccess(data) {
+    console.log('SUBSCRIPTION CREATED SUCCESSFULLY');
+}
+
+function handleSubscribeError(data) {
+    console.log('FAILED TO CREATE SUBSCRIPTION: ', data);
+}
+
+/**
+ * Platform Event Handlers
+ **/
+function handleLoginSuccess(data) {
+    // UNCOMMENT TO VIEW LOGIN DATA
+    console.log('LOGIN SUCCESS DATA: ', data.json());
+}
+
+function handleLoginError(data) {
+    console.log('LOGIN FAILURE DATA: ', data);
+}
+
+function handleLogoutSuccess(data) {
+    console.log('LOGOUT SUCCESS DATA: ', data);
+}
+
+function handleLogoutError(data) {
+    console.log('LOGOUT FAILURE DATA: ', data);
+}
+
+function handleRefreshSuccess(data) {
+    console.log('REFRESH SUCCESS DATA: ', data);
+}
+
+function handleRefreshError(data) {
+    console.log('REFRESH FAILURE DATA: ', data);
+    console.log('Initialing Login again :');
+    login();
+}
+
+server.listen(process.env.PORT);
+server.on('listening', function() {
+    console.log('Server is listening on port: ', process.env.PORT);
+});
+server.on('close', function() {
+    console.log('Server has closed and is no longer accepting connections');
+});
+server.on('error', function(err) {
+    console.error(err);
+});
+server.on('request', inboundRequest);
+
+function inboundRequest(req, res) {
+    console.log('Inbound Request');
+}
